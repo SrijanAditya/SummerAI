4d14a26394df92cfdfe4ac2a8d7a417d35b36b86
srijanaditya <srijanaditya01@gmail.com>
2024-07-02 10:06:50 +0530

    First commit

---
+++ class1/p1.s
@@ -0,0 +1,28 @@
+	.text
+	.file	"p1.c"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	movl	$0, -4(%rbp)
+	movl	$0, -8(%rbp)
+	movl	$2, -12(%rbp)
+	movl	-8(%rbp), %eax
+	addl	-12(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa %rsp, 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.ident	"Ubuntu clang version 18.1.3 (1)"
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
